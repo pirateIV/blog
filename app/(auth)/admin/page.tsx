1293fedd40bda7b2f9859065a59c8837ce0e32c6
pirateIV <benabolade@gmail.com>
2026-09-18 14:03:25 +0100

    chore: add ui draft for author's studio

--- app/(auth)/admin/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Editor() {
-  return (
-    <>
-      <h1>Editor</h1>
-    </>
-  );
-}
---
+++ app/(auth)/author/page.tsx
@@ -0,0 +1,9 @@
+import MarkdownPlayground from "@/components/author/MarkdownEditor";
+
+export default function Editor() {
+  return (
+    <>
+      <MarkdownPlayground />
+    </>
+  );
+}
